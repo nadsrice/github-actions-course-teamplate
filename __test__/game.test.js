cb6edde3de484a89004ee6e8205491b15b3b6acf
Ronald <ronald.palay@gmail.com>
2025-01-15 16:15:32 +0800

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,46 +1,108 @@
-class Game {
-  constructor(p1, p2) {
-    this.p1 = p1;
-    this.p2 = p2;
-    this.board = Array(3).fill(null).map(() => Array(3).fill(undefined)); // Empty 3x3 board
-    this.player = Math.random() < 0.5 ? p1 : p2; // Random player
-  }
+const Game = require('../src/game').default
+const fs = require('fs')
 
-  turn(row, col) {
-    if (!this.board[row][col]) {
-      this.board[row][col] = this.player === this.p1 ? 'X' : 'O'; // X for p1, O for p2
-      this.nextPlayer();
-    }
-  }
+describe('App', () => {
+  it('Contains the compiled JavaScript', async (done) => {
+    fs.readFile('./public/main.js', 'utf8', (err, data) => {
+      expect(err).toBe(null)
+      expect(data).toMatchSnapshot()
+      done()
+    })
+  })
+})
 
-  nextPlayer() {
-    this.player = this.player === this.p1 ? this.p2 : this.p1;
-  }
+describe('Game', () => {
+  let game, p1, p2
+  beforeEach(() => {
+    p1 = 'Salem'
+    p2 = 'Nate'
+    game = new Game(p1, p2)
+  })
 
-  hasWinner() {
-    // Check rows, columns, and diagonals
-    for (let r = 0; r < 3; r++) {
-      if (this.board[r][0] && this.board[r][0] === this.board[r][1] && this.board[r][1] === this.board[r][2]) {
-        return true;
+  describe('Game', () => {
+    it('Initializes with two players', async () => {
+      expect(game.p1).toBe('Salem')
+      expect(game.p2).toBe('Nate')
+    })
+
+    it('Initializes with an empty board', async () => {
+      for (let r = 0; r < game.board.length; r++) {
+        for (let c = 0; c < game.board[r].lenght; c++) {
+          expect(game.board[r][c]).toBeUndefined()
+        }
       }
-    }
+    })
 
-    for (let c = 0; c < 3; c++) {
-      if (this.board[0][c] && this.board[0][c] === this.board[1][c] && this.board[1][c] === this.board[2][c]) {
-        return true;
+    it('Starts the game with a random player', async () => {
+      Math.random = () => 0.4
+      expect(new Game(p1, p2).player).toBe('Salem')
+
+      Math.random = () => 0.6
+      expect(new Game(p1, p2).player).toBe('Nate')
+    })
+  })
+
+  describe('turn', () => {
+    it("Inserts an 'X' into the top center", async () => {
+      game.turn(0, 1)
+      expect(game.board[0][1]).toBe('X')
+    })
+
+    it("Inserts an 'X' into the top left", async () => {
+      game.turn(0)
+      expect(game.board[0][0]).toBe('X')
+    })
+  })
+
+  describe('nextPlayer', () => {
+    it('Sets the current player to be whoever it is not', async () => {
+      Math.random = () => 0.4
+      const game = new Game(p1, p2)
+      expect(game.player).toBe('Salem')
+      game.nextPlayer()
+      expect(game.player).toBe('Nate')
+    })
+  })
+
+  describe('hasWinner', () => {
+    it('Wins if any row is filled', async () => {
+      for (let r = 0; r < game.board.length; r++) {
+        for (let c = 0; c < game.board[r].length; c++) {
+          game.board[r][c] = 'X'
+        }
+        expect(game.hasWinner()).toBe(true)
+
+        for (let c = 0; c < game.board[r].length; c++) {
+          game.board[r][c] = null
+        }
       }
-    }
+    })
 
-    if (this.board[0][0] && this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2]) {
-      return true;
-    }
+    it('Wins if any column is filled', async () => {
+      for (let r = 0; r < game.board.length; r++) {
+        for (let c = 0; c < game.board[r].length; c++) {
+          game.board[c][r] = 'X'
+        }
+        expect(game.hasWinner()).toBe(true)
 
-    if (this.board[2][0] && this.board[2][0] === this.board[1][1] && this.board[1][1] === this.board[0][2]) {
-      return true;
-    }
+        for (let c = 0; c < game.board[r].length; c++) {
+          game.board[c][r] = null
+        }
+      }
+    })
 
-    return false;
-  }
-}
+    it('Wins if down-left diagonal is filled', async () => {
+      for (let r = 0; r < game.board.length; r++) {
+        game.board[r][r] = 'X'
+      }
+      expect(game.hasWinner()).toBe(true)
+    })
 
-module.exports = Game;
+    it('Wins if up-right diagonal is filled', async () => {
+      for (let r = 0; r < game.board.length; r++) {
+        game.board[2 - r][r] = 'X'
+      }
+      expect(game.hasWinner()).toBe(true)
+    })
+  })
+})
